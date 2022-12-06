d7c70847c1798d27e430f7ef320ba99c4bf7b509
Carlos Gomes <c.tcarlosantonio@gmail.com>
2022-12-06 17:03:53 +0000

    day 6 added

---
+++ day6/part1.py
@@ -0,0 +1,30 @@
+import os 
+import sys 
+import re 
+# I love Windows. 
+f = open(os.path.join(sys.path[0],"input.txt"),"r")
+l = [*f.read()]
+
+count, sum, found = 0, 0, 0
+visited = [] 
+
+for i in range(0,len(l)):
+    if found == 1:
+        break 
+    visited = [] 
+    count = 0
+    visited.append(l[i])
+    sum = i + 1
+    count += 1
+    for j in range(i + 1, len(l)):
+        if l[j] not in visited:
+            count += 1
+            visited.append(l[j])
+            sum += 1
+        else: 
+            break
+        if count == 4:
+            found = 1
+            break
+
+print(sum )
